cd0622ba8666636b43283298af9c5bbc7c1fc512
Ivan Kylchik <cilcic.v22@gmail.com>
2022-02-07 23:20:11 +0300

    Implement algorithm that will move player's parts

---
+++ Assets/Scripts/PlayersPartController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayersPartController : MonoBehaviour {
+    public GameObject player;
+    private Rigidbody rigidbody;
+    private float radius;
+    private float playerRadius;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+        radius = GetComponent<SphereCollider>().radius;
+        playerRadius = GetComponent<SphereCollider>().radius;
+    }
+
+    void Update() {
+        rigidbody.velocity = (player.transform.position - transform.position) * GetVelocityMultiplier();
+    }
+
+    private float GetVelocityMultiplier() {
+        var distance = (player.transform.position - transform.position).magnitude;
+        if (distance <= radius + playerRadius) return 0;
+        return distance * Mathf.Log(distance);
+    }
+}
