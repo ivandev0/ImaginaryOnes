8da6ab447060bad5ef36fd40b6ffa7ee36c65a33
Ivan Kylchik <cilcic.v22@gmail.com>
2022-02-23 01:47:23 +0300

    Replace cloud and boomerang stubs with real models

--- Assets/Scripts/Enemies/Boomerang.cs
+++ Assets/Scripts/Enemies/Boomerang.cs
@@ -1,34 +1,38 @@
 using UnityEngine;
 
 namespace Enemies {
-    public class Boomerang : MonoBehaviour {
-        public float speedY;
-        public float rotationSpeed;
-        public float movementRadiusX;
-        public float movementRadiusY;
+	public class Boomerang : MonoBehaviour {
+		public float speedY;
+		public float rotationSpeed;
+		public float localRotationSpeed;
+		public float movementRadiusX;
+		public float movementRadiusY;
 
-        private Vector3 center;
-        private float angle;
+		private Vector3 center;
+		private float angle;
 
-        void Start() {
-            center = transform.position;
-        }
+		void Start() {
+			center = transform.position;
+		}
 
-        void Update() {
-            var x = center.x + movementRadiusX * Mathf.Cos(angle);
-            var y = center.y + movementRadiusY * Mathf.Sin(angle);
-            center = new Vector3(center.x, center.y - Time.deltaTime * speedY * GameController.Instance.gameSpeed, 0);
-            transform.position = new Vector3( x, y, 0);
-            angle += rotationSpeed * Time.deltaTime;
-            angle %= 360;
-        }
+		void Update() {
+			var x = center.x + movementRadiusX * Mathf.Cos(angle);
+			var y = center.y + movementRadiusY * Mathf.Sin(angle);
+			center = new Vector3(center.x, center.y - Time.deltaTime * speedY * GameController.Instance.gameSpeed, 0);
+			transform.position = new Vector3(x, y, 0);
+			angle += rotationSpeed * Time.deltaTime;
+			angle %= 360;
 
-        private void OnTriggerEnter(Collider other) {
-            if (other.gameObject.CompareTag("Player")) {
-                GameController.Instance.GameOver();
-            } else if (other.gameObject.CompareTag("PlayerPart")) {
-                other.gameObject.GetComponent<PlayerPart>().BlowUp(EnemyType.Boomerang, () => { Destroy(other.gameObject); });
-            }
-        }
-    }
+			transform.Rotate(Vector3.up * (localRotationSpeed * Time.deltaTime));
+		}
+
+		private void OnTriggerEnter(Collider other) {
+			if (other.gameObject.CompareTag("Player")) {
+				GameController.Instance.GameOver();
+			} else if (other.gameObject.CompareTag("PlayerPart")) {
+				other.gameObject.GetComponent<PlayerPart>()
+					.BlowUp(EnemyType.Boomerang, () => { Destroy(other.gameObject); });
+			}
+		}
+	}
 }
